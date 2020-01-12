bad074720c9a638339b5d0afdeab0074e072f1f5
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-01-12 02:01:15 +0100

    build: add initial conan files

---
+++ README.md
@@ -0,0 +1,41 @@
+# conan-bacnetstack
+
+[![Conan](https://api.bintray.com/packages/gocarlos/public-conan/bacnetstack:gocarlos/images/download.svg) ](https://bintray.com/gocarlos/public-conan/bacnetstack:gocarlos/_latestVersion)
+
+## lib producer
+
+```bash
+# build conan package
+conan create . gocarlos/testing --build missing
+# upload package to gocarlos artifactory
+conan upload bacnetstack -r gocarlos
+```
+
+## lib consumer
+
+```bash
+conan remote add gocarlos https://api.bintray.com/conan/gocarlos/public-conan
+```
+
+add to your conanfile.txt
+
+```toml
+[requires]
+bacnetstack/4.3.0@gocarlos/testing
+
+[generators]
+cmake_find_package
+cmake_paths
+
+[options]
+# if without openssl
+```
+
+add to your CMakeLists.txt
+
+```cmake
+find_package(bacnetstack REQUIRED)
+#...
+add_executable(main main.cpp)
+target_link_libraries(main PRIVATE bacnetstack)
+```
---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, CMake, tools
+
+
+class BacnetStackConan(ConanFile):
+    name = "bacnetstack"
+    version = "4.3.0"
+    license = "BSD"
+    author = "Carlos Gomes Martinho kmartinho8@gmail.com"
+    url = "https://github.com/bacnet-stack/bacnet-stack/"
+    description = "BACnet Protocol Stack library provides a BACnet application layer, network layer and media access (MAC) layer communications services."
+    topics = ("bacnet")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False]
+    }
+    default_options = {
+        "shared": False
+    }
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/gocarlos/bacnet-stack.git")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="bacnet-stack")
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["bacnet-stack"]
+        self.cpp_info.name = "bacnet-stack"
